1f7c90519cd5ffbef55f5e49807d7cfb6f96403c
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-07 07:52:16 -0400

    Complete value function tests

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -11,15 +11,57 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   /////////////////////////////////////////////////////
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits examples") {
+  test("selectDigits examples") {
     assert(selectDigits("hello4world-80") == "480")
     assert(selectDigits("welcome") == "")
   }
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits length is smaller") {
+  test("selectDigits length is smaller") {
     forAll { (text: String) =>
       assert(selectDigits(text).length <= text.length)
+    }
+  }
+
+  test("selectDigits chars are all digits") {
+    forAll { (text: String) =>
+      selectDigits(text).foreach(c => assert(c.isDigit))
+    }
+  }
+
+  test("secret examples") {
+    assert(secret("Welcome123") == "**********")
+  }
+
+  test("secret length is the same") {
+    forAll { (text: String) =>
+      assert(secret(text).length == text.length)
+    }
+  }
+
+  test("secret chars are all asterisks") {
+    forAll { (text: String) =>
+      secret(text).foreach(c => assert(c == '*'))
+    }
+  }
+
+  test("secret idempotence") {
+    forAll { (text: String) =>
+      val once = secret(text)
+      val twice = secret(secret(text))
+      assert(once == twice)
+    }
+  }
+
+  test("isValidUsernameCharacter examples") {
+    assert(isValidUsernameCharacter('3'))
+    assert(isValidUsernameCharacter('a'))
+    assert(!isValidUsernameCharacter('^'))
+  }
+
+  test("isValidUsername") {
+    forAll { (username: String) =>
+      assert(isValidUsername(username.reverse) == isValidUsername(username))
     }
   }
 
@@ -27,4 +69,21 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   // Exercise 2: Point
   ///////////////////////
 
+  test("Point isPositive max 0") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(Point(x.max(0), y.max(0), z.max(0)).isPositive)
+    }
+  }
+
+  test("Point isEven product 2") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(Point(x * 2, y * 2, z * 2).isEven)
+    }
+  }
+
+  test("Point forAll") {
+    forAll { (x: Int, y: Int, z: Int, predicate: Int => Boolean) =>
+      assert(Point(x,y,z).forAll(predicate) == List(x,y,z).forall(predicate))
+    }
+  }
 }
